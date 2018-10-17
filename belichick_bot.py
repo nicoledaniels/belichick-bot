0b8ff4affa7c32f785768902c0634a26260b3a13
Nicole Daniels <nzoner12@yahoo.com>
2018-10-16 22:35:20 -0400

    Generate an index for a random quote

--- belichick_bot.py
+++ belichick_bot.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+# Weird encoding stuff
+
 import praw
 import re
 import os
-import time
+import random
 
 # Create the Reddit instance and log in
 reddit = praw.Reddit('bot')
+
+# Iconic quotes
+billsQuotes=["I don’t Twitter, I don’t MyFace, I don’t Yearbook","Twitter account, InstantFace, I don’t have any of that","I don’t know. MyFace, YourFace, InstantFace","Not on SnapFace, not too worried what they put on InstaChat","You get the job done or you don’t","Do your job","Knowing you have a good backup long snapper allows you to sleep good at night","What the hell is that?","Well, umm…he’s a good player. We have a lot of good players","I’m a football coach. I’m not a doctor","We're on to Cincinnati","You Sure That’s the Question You Want to Ask?","We did what we did","What do you think?","I don’t know","They were inactive so they didn't play","Seattle. Seattle. Seattle. Seattle. Seattle"]
 
 # Create a list
 if not os.path.isfile("comments_replied_to.txt"):
@@ -23,8 +29,16 @@ for comment in subreddit.comments(limit=1000):
 
     # Make sure you didn't already reply to this comment and it includes the provided text
     if comment.id not in comments_replied_to and re.search("belichick says", comment.body, re.IGNORECASE):
-      comment.reply("bot responding!")
+      # Generate a random quote number
+      random_quote_index = random.randint(0, len(billsQuotes) - 1)
+
+      # Set the random quote
+      quote = billsQuotes[random_quote_index]
+
+      bot_signature_text = "^(I'm a bot! Message me if you have a spicy Belichick quote you want to add)"
+
       print("Bot replying to : ", comment.body)
+      comment.reply(quote + "\n" + "***" + "\n" + bot_signature_text)
 
       # Store id in list
       comments_replied_to.append(comment.id)
